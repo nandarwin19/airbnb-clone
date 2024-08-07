ce8af8e2406e2afef5ae9695003d644d9634ca8c
nwin19 <nandarwinbuma@gmail.com>
2024-08-07 17:42:42 +0630

    feat: header

---
+++ app/rentals/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RentalsPage() {
+  return <div>RentalsPage</div>;
+}
---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+export default function NavSearch() {
+  return (
+    <Input
+      type="text"
+      placeholder="find a property"
+      className="max-w-xs dark:bg-muted"
+    />
+  );
+}
