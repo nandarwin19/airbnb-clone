50e79b02defdd38c30fefbd340d683f966bbbb45
nwin19 <nandarwinbuma@gmail.com>
2024-08-08 13:37:39 +0630

    feat: add formcontainer, profile create page, form input components

---
+++ components/form/FormInput.tsx
@@ -0,0 +1,31 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+type FormInputProps = {
+  name: string;
+  type: string;
+  label?: string;
+  defaultValue?: string;
+  placeholder?: string;
+};
+
+function FormInput(props: FormInputProps) {
+  const { label, name, type, defaultValue, placeholder } = props;
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        {label || name}
+      </Label>
+      <Input
+        type={type}
+        name={name}
+        id={name}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
+
+export default FormInput;
