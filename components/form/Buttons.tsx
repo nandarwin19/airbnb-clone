bb0ed28e48281fcdb015ca5b915af07dde1477d2
nwin19 <nandarwinbuma@gmail.com>
2024-08-15 16:36:58 +0630

    feat: add navsearch

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,39 +1,19 @@
-"use client";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { Button } from "../ui/button";
 
-import { ReloadIcon } from "@radix-ui/react-icons";
-import { useFormStatus } from "react-dom";
-import { Button } from "@/components/ui/button";
-
-type btnSize = "default" | "sm" | "lg";
-
-type SubmitButtonProps = {
-  className?: string;
-  text?: string;
-  size?: btnSize;
-};
-
-export function SubmitButton({
-  className = "",
-  text = "submit",
-  size = "lg",
-}: SubmitButtonProps) {
-  const { pending } = useFormStatus();
-
+export const CardSignInButton = () => {
   return (
-    <Button
-      type="submit"
-      disabled={pending}
-      className={`capitalize ${className}`}
-      size={size}
-    >
-      {pending ? (
-        <>
-          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-          Please wait...
-        </>
-      ) : (
-        text
-      )}
-    </Button>
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
   );
-}
+};
