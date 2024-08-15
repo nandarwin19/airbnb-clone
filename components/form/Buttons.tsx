5a04e0e3b6f1856f0eb825d668045f8e23dfbe03
nwin19 <nandarwinbuma@gmail.com>
2024-08-15 17:10:41 +0630

    feat: add fav toggle button and logic

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,6 +1,64 @@
+"use client";
+
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { SignInButton } from "@clerk/nextjs";
-import { FaRegHeart, FaHeart } from "react-icons/fa";
-import { Button } from "../ui/button";
+
+type btnSize = "default" | "sm" | "lg";
+
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+export function SubmitButton({
+  className = "",
+  text = "submit",
+  size = "lg",
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      disabled={pending}
+      className={`capitalize ${className}`}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="outline"
+      className=" p-2 cursor-pointer"
+    >
+      {pending ? (
+        <ReloadIcon className=" animate-spin" />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
+  );
+};
 
 export const CardSignInButton = () => {
   return (
