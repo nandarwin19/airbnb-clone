a1c0e07fe5c1598079e89d207bf2668a4fb91967
nwin19 <nandarwinbuma@gmail.com>
2024-08-15 19:10:12 +0630

    feat: add fav page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import EmptyList from "@/components/home/EmptyList";
+import PropertiesList from "@/components/home/PropertiesList";
+import { fetchFavorites } from "@/utils/actions";
 
-export default function FavoritesPage() {
-  return (
-    <div>FavoritesPage</div>
-  )
+async function FavoritesPage() {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0) {
+    return <EmptyList />;
+  }
+
+  return <PropertiesList properties={favorites} />;
 }
+export default FavoritesPage;
