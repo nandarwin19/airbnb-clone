6c4d9108b2baba4e3ee7004bcfad83de7c2840b6
nwin19 <nandarwinbuma@gmail.com>
2024-08-18 23:56:56 +0630

    feat: add reviews page, review card logic

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -5,6 +5,7 @@ import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { SignInButton } from "@clerk/nextjs";
+import { LuPenSquare, LuTrash2 } from "react-icons/lu";
 
 type btnSize = "default" | "sm" | "lg";
 
@@ -73,5 +74,34 @@ export const CardSignInButton = () => {
         <FaRegHeart />
       </Button>
     </SignInButton>
+  );
+};
+
+type actionType = "edit" | "delete";
+
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus();
+
+  const renderIcon = () => {
+    switch (actionType) {
+      case "edit":
+        return <LuPenSquare />;
+      case "delete":
+        return <LuTrash2 />;
+      default:
+        const never: never = actionType; //
+        throw new Error(`Invalid action type: ${never}`);
+    }
+  };
+
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="link"
+      className="p-2 cursor-pointer"
+    >
+      {pending ? <ReloadIcon className=" animate-spin" /> : renderIcon()}
+    </Button>
   );
 };
--- utils/actions.ts
+++ utils/actions.ts
@@ -290,14 +290,65 @@ export async function createReviewAction(prevState: any, formData: FormData) {
   }
 }
 
-export const fetchPropertyReviews = async () => {
-  return { message: "fetch reviews" };
-};
+export async function fetchPropertyReviews(propertyId: string) {
+  const reviews = await db.review.findMany({
+    where: {
+      propertyId,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      profile: {
+        select: {
+          firstName: true,
+          profileImage: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return reviews;
+}
 
 export const fetchPropertyReviewsByUser = async () => {
-  return { message: "fetch user reviews" };
+  const user = await getAuthUser();
+  const reviews = await db.review.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      property: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+  return reviews;
 };
 
-export const deleteReviewAction = async () => {
-  return { message: "delete  reviews" };
+export const deleteReviewAction = async (prevState: { reviewId: string }) => {
+  const { reviewId } = prevState;
+  const user = await getAuthUser();
+
+  try {
+    await db.review.delete({
+      where: {
+        id: reviewId,
+        profileId: user.id,
+      },
+    });
+
+    revalidatePath("/reviews");
+    return { message: "Review deleted successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
