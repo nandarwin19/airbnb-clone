362c21126e554d62744f5fa8b604dce296a77b49
nwin19 <nandarwinbuma@gmail.com>
2024-08-23 17:25:05 +0630

    feat: add zustand library

--- components/booking/BookingWrapper.tsx
+++ components/booking/BookingWrapper.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
 
-const BookingWrapper = () => {
+import { useProperty } from "@/utils/store";
+import { Booking } from "@/utils/types";
+import BookingCalendar from "./BookingCalendar";
+import BookingContainer from "./BookingContainer";
+import { useEffect } from "react";
+
+type BookingWrapperProps = {
+  propertyId: string;
+  price: number;
+  bookings: Booking[];
+};
+export default function BookingWrapper({
+  propertyId,
+  price,
+  bookings,
+}: BookingWrapperProps) {
+  useEffect(() => {
+    useProperty.setState({
+      propertyId,
+      price,
+      bookings,
+    });
+  }, []);
   return (
-    <div>BookingWrapper</div>
-  )
+    <>
+      <BookingCalendar />
+      <BookingContainer />
+    </>
+  );
 }
-
-export default BookingWrapper
