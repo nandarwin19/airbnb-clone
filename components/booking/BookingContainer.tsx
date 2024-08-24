6d0d68cce81acf24b6043d5a1bce2325f748dda2
nwin19 <nandarwinbuma@gmail.com>
2024-08-24 10:15:28 +0630

    feat: add book form

--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -4,13 +4,11 @@ import { useProperty } from "@/utils/store";
 import ConfirmBooking from "./ConfirmBooking";
 import BookingForm from "./BookingForm";
 function BookingContainer() {
-  // const { range } = useProperty((state) => state);
+  const { range } = useProperty((state) => state);
 
-  // if (!range || !range.from || !range.to) return null;
-  // if (range.to.getTime() === range.from.getTime()) return null;
-  
-  const state = useProperty((state) => state);
-  console.log(state)
+  if (!range || !range.from || !range.to) return null;
+  if (range.to.getTime() === range.from.getTime()) return null;
+
 
   
   return (
