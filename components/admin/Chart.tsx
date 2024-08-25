940dd612f07f61ca8ffff15d52c509cc8d5190ca
nwin19 <nandarwinbuma@gmail.com>
2024-08-25 19:54:53 +0630

    feat: chart container

--- components/admin/Chart.tsx
+++ components/admin/Chart.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-const Chart = () => {
+type ChartPropsType = {
+  data: {
+    date: string;
+    count: number;
+  }[];
+};
+
+function Chart({ data }: ChartPropsType) {
   return (
-    <div>Chart</div>
-  )
+    <section className="mt-24">
+      <h1 className="text-4xl font-semibold text-center">Monthly Bookings</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey="count" fill="#F97215" barSize={75} />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
 }
-
-export default Chart
+export default Chart;
