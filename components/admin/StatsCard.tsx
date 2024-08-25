5f70a855ff4ecdfa96b9614dbd89185a9e30f113
nwin19 <nandarwinbuma@gmail.com>
2024-08-25 16:53:09 +0630

    feat: add stats container

--- components/admin/StatsCard.tsx
+++ components/admin/StatsCard.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Card, CardHeader } from "@/components/ui/card";
 
-const StatsCard = () => {
+type StatsCardsProps = {
+  title: string;
+  value: number;
+};
+
+function StatsCards({ title, value }: StatsCardsProps) {
   return (
-    <div>StatsCard</div>
-  )
+    <Card className="bg-muted">
+      <CardHeader className="flex flex-row justify-between items-center">
+        <h3 className="capitalize text-3xl font-bold">{title}</h3>
+        <span className="text-primary text-5xl font-extrabold">{value}</span>
+      </CardHeader>
+    </Card>
+  );
 }
 
-export default StatsCard
+export default StatsCards;
