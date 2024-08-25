93253fe1100a6e7579c6df1d9ca0feeb4d8c0769
nwin19 <nandarwinbuma@gmail.com>
2024-08-25 16:45:48 +0630

    feat: add loading and fetch stas logic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-const AdminPage = () => {
+import ChartsContainer from "@/components/admin/ChartsContainer";
+import StatsContainer from "@/components/admin/StatsContainer";
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContainer,
+} from "@/components/admin/Loading";
+import { Suspense } from "react";
+async function AdminPage() {
   return (
-    <div>AdminPage</div>
-  )
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  );
 }
-
-export default AdminPage
+export default AdminPage;
